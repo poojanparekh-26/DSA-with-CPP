e4af754aaa08e4abf16f91ec3cdb28c757d64a31
Poojan Parekh <poojanparekh001@gmail.com>
2026-03-19 23:40:16 +0530

    Codeforces Day 2: Solve 467A (George and Accommodation) and 677A (Vanya and Fence) using simple conditional counting

---
+++ Codeforces_Questions/George_and_Accomodation(467A).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int p[n],q[n];
+    int count = 0;
+    for(int i=0;i<n;i++){
+        cin >> p[i] >> q[i];
+        if(q[i]-p[i]>1) count++;
+    }
+    cout << count;
+}
