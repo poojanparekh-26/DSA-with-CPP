387fcd3bba18f1315007a24210c3986299b0f1a6
Poojan Parekh <poojanparekh001@gmail.com>
2026-03-26 23:57:03 +0530

    solve: Codeforces 41A - Check if second string is reverse of first using STL reverse()

---
+++ Codeforces_Questions/Even_Odds(318A).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    long long oddCount = (n + 1) / 2;
+
+    if(k <= oddCount)
+        cout << 2*k - 1;
+    else
+        cout << 2*(k - oddCount);
+
+    return 0;
+}
