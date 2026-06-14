d71c39c4b472fa745103fef0800f3f186dc3bffa
Poojan Parekh <poojanparekh001@gmail.com>
2026-06-14 12:53:53 +0530

    feat(cp31-1200): solve problem 15 (CF 1692E Binary Deque)

---
+++ Codeforces/CP31 (1200)/15_Binary_Deque(1692E).cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, s;
+        cin >> n >> s;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        int sum = 0;
+        int l = 0;
+        int mx = -1;
+        for(int r = 0; r < n; r++){
+            sum += a[r];
+            while(sum > s){
+                sum -= a[l];
+                l++;
+            }
+            if(sum == s) mx = max(mx, r - l + 1);
+        }
+        if(mx == -1) cout << -1 << endl;
+        else cout << n - mx << endl;
+    }
+
+    return 0;
+}
