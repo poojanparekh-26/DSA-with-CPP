657fd1e2102f7b425a99e1df647797a40a9bb20d
Poojan Parekh <poojanparekh001@gmail.com>
2026-07-13 23:39:25 +0530

    refactor(leetcode-jul2026): optimize LC 1 Two Sum solution

--- Hashing/1_Two_Sum.cpp
+++ Hashing/1_Two_Sum.cpp
@@ -7,19 +7,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int,int> mp;
-
-    for(int i = 0; i < nums.size(); i++){
-        int complement = target - nums[i];
-
-        if(mp.count(complement))
-            return {mp[complement], i};
-
-        mp[nums[i]] = i;
-    }
-
-    return {};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int rem = target - nums[i];
+            if(mp.count(rem)){
+                return {mp[rem],i};
+            }
+            mp[nums[i]] = i;;
+        }
+        return {};
 }
 
 int main() {
